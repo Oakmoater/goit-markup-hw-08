b058e5aa1bdefa8699c9bcbb34261d8326be65bb
Oakmoater <dubrovecva@gmail.com>
2022-12-17 12:37:17 +0100

    tablet

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -9,6 +9,7 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("menu_open");
     refs.modal.classList.toggle("is-hidden");
   }
 })();
